1e3803a9be38ced5528c6c10d0823fda7768d8c9
chowx <chowx@MAIZIE-GPU2.vuds.vanderbilt.edu>
2026-06-26 09:21:57 -0500

    fix: map deprecated sparsity fold-change to logit shift

--- tests/test_marginal_alteration.py
+++ tests/test_marginal_alteration.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from FEAST.modeling.StudentT_mixture_model import StudentTMixtureMarginalModeler
-from FEAST.modeling.marginal_alteration import alter_marginal_model
+from FEAST.modeling.marginal_alteration import AlterationConfig, alter_marginal_model
 
 
 def test_interpolated_studentt_ppf_refreshes_after_mean_alteration():
@@ -27,3 +27,15 @@ def test_interpolated_studentt_ppf_refreshes_after_mean_alteration():
 
     np.testing.assert_allclose(after[1:3] / before[1:3], 2.0, rtol=0.15)
     assert after[-1] > model.data_range[1]
+
+
+def test_deprecated_sparsity_fold_change_maps_to_logit_shift():
+    decreased = AlterationConfig.sparsity_only(fold_change=0.5)
+    increased = AlterationConfig.sparsity_only(fold_change=2.0)
+
+    assert decreased.apply_to_zero_prop
+    assert increased.apply_to_zero_prop
+    assert decreased.sparsity_logit_shift < 0
+    assert increased.sparsity_logit_shift > 0
+    np.testing.assert_allclose(decreased.sparsity_logit_shift, np.log(0.5))
+    np.testing.assert_allclose(increased.sparsity_logit_shift, np.log(2.0))
